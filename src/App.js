43e7ee50bcddb0b2bc254e24043ba9a6454df430
Divyesh Mehta <divyeshmehta23@yahoo.com>
2023-05-07 15:48:25 -0700

    Adding alert to the app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Textarea from './components/Textarea';
 import React from 'react'
 import './App.css';
 import { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const toggleModeFunction = () => {
@@ -12,21 +13,34 @@ function App() {
     setModetext("Enable Light Mode")
     document.body.style.color = "white"
     document.body.style.backgroundColor = "black"
+    showAlert("Enabling dark mode", "success")
     }
     else{
       setMode("light")
       setModetext("Enable Dark Mode")
       document.body.style.color = "black"
       document.body.style.backgroundColor = "white"
+      showAlert("Enabling Light mode", "success")
       
     }
   }
+ const showAlert = (message, type) => {
+    setAlert({
+      msg : message, 
+      type : type,
+    });
+    setTimeout(() => {
+      setAlert(null)
+  }, 1500)
+  }
+  const [alert, setAlert] = useState(null)
   const [mode, setMode] = useState("light");
   const [modeText, setModetext] = useState("Enable Dark Mode");   
   return (
     <>
       <Navbar appName="Word Editor" mode={mode} toggleMode={toggleModeFunction} setText={modeText}/>
-      <Textarea/>
+      <Alert alert={alert}/>
+      <Textarea showAlert={showAlert}/>
       {/* <About/> */}
     </>
   );
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,32 +1,38 @@
 import React, { memo, useState } from "react";
 
-export default memo(function Textarea() {
+export default memo(function Textarea(props) {
     const textAreaChanged = (event)=>{
         setText(event.target.value)
     }
     const changeToUppercase = ()=>{
         let newText = text.toUpperCase();
-        setText(newText)
+        setText(newText);
+        props.showAlert("Text is changed to Uppercase", "success")
     }
     const changeToLowercase = ()=>{
         let newText = text.toLowerCase();
-        setText(newText)
+        setText(newText); 
+        props.showAlert("Text is changed to Lowercase", "success")
     }
     const capitalizeFirstLetter = ()=>{
         let newText = text.split(" ").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
-        setText(newText)
+        setText(newText);
+        props.showAlert("Firt Letter of each word is Capitalized", "success")
     }
     const copyText = ()=> {
         let textAreaElement = document.getElementById("exampleFormControlTextarea1");
         textAreaElement.select();
         navigator.clipboard.writeText(textAreaElement.value);
+        props.showAlert("Text is copied to Clipboard", "success")
     }
     const removeExtraSpace = ()=> {
         let newText = text.split(/[ ]+/);
-        setText(newText.join(" "))
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces are removed from the text", "success")
     }
     const clearText = ()=>{
-        setText("")
+        setText("");
+        props.showAlert("Text is cleared", "success")
     }
     const[text, setText]=useState("")
   return (
@@ -42,8 +48,8 @@ export default memo(function Textarea() {
             rows="8"
             placeholder="Enter your text here"
             value={text}
-            onChange={event => textAreaChanged(event)}
-          ></textarea>
+            onChange={event => textAreaChanged(event)}>
+            </textarea>
         </div>
         <div>
           <button className="btn btn-primary mx-3" onClick={changeToUppercase}>Change to Uppercase</button>
@@ -53,7 +59,7 @@ export default memo(function Textarea() {
           <button className="btn btn-primary mx-3" onClick={removeExtraSpace}>Remove Extra spaces</button>
           <button className="btn btn-primary mx-3" onClick={clearText}>Clear Text</button>
         </div>
-         <p className="my-2">word count = {text.split(" ").length}</p>
+         <p className="my-2">word count = {text==="" ? 0: text.split(" ").length}</p>
          <p className="my-2">char count = {text.length}</p>
          <h2 className="my-4">Preview</h2>
          <p>{text.length>0 ? text : "Enter the text in text area above to preivew"}</p>
